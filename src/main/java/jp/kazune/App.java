4f3739f04947658e7391a3a79d3b7fa9257df915
kazu <kazu@tosiki-no-MacBook-Air.local>
2017-03-07 22:27:48 +0900

    first commit.

---
+++ src/main/java/jp/kazune/App.java
@@ -0,0 +1,13 @@
+package jp.kazune;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
